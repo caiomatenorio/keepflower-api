eab04e769788ce79083c1a0373c3d4c36364b299
Caio Tenório <caiomatenorio@gmail.com>
2025-07-01 18:25:09 -0300

    feat: comment out unused authentication and user endpoint methods

--- src/main/java/com/keepflower/api/controller/UserController.java
+++ src/main/java/com/keepflower/api/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.keepflower.api.controller;
 
-import com.keepflower.api.common.response.ResponseBody;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/users")
 public class UserController {
-    @GetMapping("/me")
-    public ResponseEntity<ResponseBody> getCurrentUser() {
-    }
+//    @GetMapping("/me")
+//    public ResponseEntity<ResponseBody> getCurrentUser() {
+//    }
 }
