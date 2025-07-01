d2f04a8fc688f1c77af9bd80b5116ca59820e27c
Caio Tenório <caiomatenorio@gmail.com>
2025-07-01 18:15:51 -0300

    fix: correct RestController annotation usage in PingController

--- src/main/java/com/keepflower/api/controller/PingController.java
+++ src/main/java/com/keepflower/api/controller/PingController.java
@@ -4,9 +4,11 @@ import com.keepflower.api.common.response.ResponseBody;
 import com.keepflower.api.common.response.SuccessResponseBody;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/ping")
+@RestController
+@RequestMapping("/ping")
 public class PingController {
     @GetMapping
     public ResponseEntity<ResponseBody> ping() {
