ed4f58e092732e3d87f1b7dbc25cdd8e78045499
Caio Tenório <caiomatenorio@gmail.com>
2025-07-08 12:42:32 -0300

    feat: add basic models for users and sessions

--- src/main/java/com/keepflower/api/model/Session.java
+++ src/main/java/com/keepflower/api/model/Session.java
@@ -1,4 +1,43 @@
 package com.keepflower.api.model;
 
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "sessions")
+@Data
+@NoArgsConstructor
 public class Session {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private User user;
+
+    @Column(nullable = false, unique = true)
+    private String refreshToken;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private Instant updatedAt;
 }
--- src/main/java/com/keepflower/api/model/User.java
+++ src/main/java/com/keepflower/api/model/User.java
@@ -1,4 +1,33 @@
 package com.keepflower.api.model;
 
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @OneToMany(mappedBy = "user")
+    private Set<Session> sessions;
 }
