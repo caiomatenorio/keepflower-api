d06553d5c28edbf6d5580a2e218f74bdda49beec
Caio Tenório <caiomatenorio@gmail.com>
2025-07-14 16:06:51 -0300

    feat: implement UserController with sign-up endpoint and create SignUpRequest DTO

--- src/main/java/com/keepflower/api/controller/UserController.java
+++ src/main/java/com/keepflower/api/controller/UserController.java
@@ -1,10 +1,26 @@
 package com.keepflower.api.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/users")
+import com.keepflower.api.common.response.ResponseBody;
+import com.keepflower.api.common.response.SuccessResponseBody;
+import com.keepflower.api.dto.request.SignUpRequest;
+import com.keepflower.api.service.UserService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
-    // @GetMapping("/me")
-    // public ResponseEntity<ResponseBody> getCurrentUser() {
-    // }
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<ResponseBody> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
+        userService.createUser(signUpRequest.username(), signUpRequest.password());
+        return ResponseEntity.ok(new SuccessResponseBody<>("User created successfully"));
+    }
 }
